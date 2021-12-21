81ccca38b67f10ca23a035cdc1ad103a573a1ad5
tehrelt <haxrelt@gmail.com>
2021-12-21 22:08:19 +0300

    Оптимальненько FIX

--- L9/Source.c
+++ L9/Source.c
@@ -1,4 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define LINE "============================================================"
+#define Culture struct Culture 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,22 +8,22 @@
 #include <string.h>
 #include <locale.h>
 
+
 void FileCreate();
-void DisplayFileContent();
-void ReverseAlphabetSort();
-int GetMaxCulture();
-int GetCultures();
+void AnalyzeFileContent();
+void DisplayFileContent(char name[13]);
 int ValidateINT();
 
-FILE* file;
-int N = 0;
-int years[] = { 1960, 1967 };
-
-struct Culture
+Culture
 {
 	char name[60];
 	int area[2];
-} e;
+};
+
+FILE* file;
+Culture e;
+int years[] = { 1960, 1967 };
+
 
 
 
@@ -35,17 +37,14 @@ int main()
 
 	while (1)
 	{		
-		printf("Что нужно сделать с файлом?\n");
+		printf("%s\nЧто нужно сделать с файлом?\n", LINE);
 		printf("1 - Создать файл\n");
 		printf("2 - Вывод содержимого\n");
-		printf("3 - Узнать культуру и её максимальную площадь в 1960г.\n");
-		printf("4 - Узнать культуры площадь которых превысила 1000 т. га в 1967г.\n");
-		printf("5 - Упорядочить в обратном порядке название культур, чья площадь в 1960г. не превысила 1300га.\n");
 		printf("0 - Выход\n");
 		printf("Ваш выбор: ");
 		choice = ValidateINT();
 		printf("\n\n");
-		while (choice > 5 || choice < 0)
+		while (choice > 3 || choice < 0)
 		{
 			printf("Ошибка, такой категории нет. Повторите попытку!\n");
 			printf("Ваш выбор: ");
@@ -55,17 +54,25 @@ int main()
 		{
 		case 1:	FileCreate();
 			break;
-		case 2: DisplayFileContent("res.dat");
+		case 2: 
+			AnalyzeFileContent();
+			printf("\t\tres.dat: \n");
+			DisplayFileContent("res.dat");
+
+			printf("\n\t\tMaxArea.dat: \n");
+			DisplayFileContent("MaxArea.dat");
+
+			printf("\n\t\OverThousandArea.dat: \n");
+			DisplayFileContent("OverThousandArea.dat");
+
+			printf("\n\t\tSorted.dat: \n");
+			DisplayFileContent("Sorted.dat");
 			break;
-		case 3: GetMaxCulture();
-			break;
-		case 4: GetCultures();
-			break;
-		case 5: ReverseAlphabetSort();
-			break;
-		case 0: return 0;
+		case 0: 
+			system("pause");
+			return 0;
 		}
-		printf("\n\n");
+		printf("%s\n\n", LINE);
 	}
 }
 
@@ -85,7 +92,6 @@ void FileCreate()
 		gets(e.name);
 		if (strcmp(e.name, "") != 0)
 		{
-			N++;
 			fwrite(&e.name, sizeof(e.name), 1, file);
 			for (int i = 0; i < 2; i++)
 			{
@@ -123,128 +129,53 @@ void DisplayFileContent(char name[13])
 	}
 	fclose(file);
 }
-void ReverseAlphabetSort()
+void AnalyzeFileContent()
 {
-	struct Culture sorted[30];
-	FILE* sortedFile;
-	unsigned char c1, c2;
-	char temp[30];
-	int tempInt;
-	int limit = 1300;
-	int len, i, j, k, u, v;
-
-	if ((file = fopen("res.dat", "r"))				== NULL || 
-		(sortedFile = fopen("sorted.dat", "wb+"))	== NULL)
-	{
-		printf("Ошибка открытия файла\n");
-		exit(-1);
-	}
-
-	fseek(file, 0, SEEK_END);
-
-	len = ftell(file);
-	i = 0;
-	j = 0;
-
-	while (i < len)
-	{
-		printf("i = %d\n", i);
-		fseek(file, i, SEEK_SET);
-		fread(&sorted[j].name, sizeof(e.name), 1, file);
-		fread(&sorted[j].area[0], sizeof(int), 1, file);
-		fread(&sorted[j].area[1], sizeof(int), 1, file);
-		j++;
-		i += sizeof(e.name) + 2 * sizeof(int);
-	}
-
-	/*for (k = 0; k < j; k++)
-	{
-		printf("sorted[%d] = { %s, %d, %d }\n", k, sorted[k].name, sorted[k].area[0], sorted[k].area[1]);
-	}*/
-
-	printf("j = %d\n", j);
-	for (k = 0; k < j; k++)
-	{
-		for (u = j-1; u >= k; u--)
-		{
-			c1 = sorted[u - 1].name[0];
-			c2 = sorted[u].name[0];
-			if (c1 > c2)
-			{
-				strcpy(temp, sorted[u - 1].name);
-				strcpy(sorted[u - 1].name, sorted[u].name);
-				strcpy(sorted[u].name, temp);
-				for (v = 0; v < 2; v++)
-				{
-					tempInt = sorted[u - 1].area[v];
-					sorted[u - 1].area[v] = sorted[u].area[v];
-					sorted[u].area[v] = tempInt;
-				}
-			}
-		}
-	}
-	for (i = 0; i < j; i++)
-	{
-		fwrite(&sorted[i].name, sizeof(sorted[i].name), 1, sortedFile);
-		fwrite(&sorted[i].area[0], sizeof(int), 1, sortedFile);
-		fwrite(&sorted[i].area[1], sizeof(int), 1, sortedFile);
-	}
-
-	fclose(file);
-	fclose(sortedFile);
-	DisplayFileContent("sorted.dat");
-}
-int GetMaxCulture() // Макс. площадь в 1960г.
-{
+#pragma region Variables
 	int maxArea = 0;
 	char maxAreaCulture[50];
-	int len, i;
+
+	unsigned char c1, c2;
+
+	int len, 
+		i, 
+		j = 0, 
+		t, 
+		tempName[30];
+#pragma endregion
+
+	Culture sorted[30];
+
+	FILE* MaxArea,
+		* OverThousandArea,
+		* Sorted;
+
 	if ((file = fopen("res.dat", "r")) == NULL)
 	{
 		printf("Ошибка открытия файла\n");
 		exit(-1);
 	}
+	if ((MaxArea = fopen("MaxArea.dat", "wb+")) == NULL)
+	{
+		printf("Ошибка открытия файла\n");
+		exit(-1);
+	}
+	if ((OverThousandArea = fopen("OverThousandArea.dat", "wb+")) == NULL)
+	{
+		printf("Ошибка открытия файла\n");
+		exit(-1);
+	}
+	if ((Sorted = fopen("Sorted.dat", "wb+")) == NULL)
+	{
+		printf("Ошибка открытия файла\n");
+		exit(-1);
+	}
+
 	fseek(file, 0, SEEK_END);
 	len = ftell(file);
 	i = 0;
 
-	while (i < len)
-	{
-		fseek(file, i, SEEK_SET);
-		fread(&e.name, sizeof(e.name), 1, file);
-		fread(&e.area[0], sizeof(int), 1, file);
-		if (i == 0)
-		{
-			strcpy(maxAreaCulture, e.name);
-			maxArea = e.area[0];
-		}
-		else if (e.area[0] > maxArea)
-		{
-			strcpy(maxAreaCulture, e.name);
-			maxArea = e.area[0];
-		}
-		fread(&e.area[1], sizeof(int), 1, file);	
-		i += sizeof(e.name) + 2 * sizeof(int);
-		//printf("\n e.area = %d, maxArea = %d", e.area[0], maxArea);
-	}
-	printf("Max Area is equal to %d in %s ", maxArea, maxAreaCulture);
-	fclose(file);
-	return 0;
-}
-int GetCultures()
-{
-	int currentArea = 0;
-	int limit = 1000;
-	char currentCulture[50];
-	int len, i;
-	if ((file = fopen("res.dat", "r")) == NULL)
-	{
-		printf("Ошибка открытия файла\n");
-		exit(-1);
-	}
-	fseek(file, 0, SEEK_END);
-	len = ftell(file);
-	i = 0;
+	strcpy(maxAreaCulture, "a");
 
 	while (i < len)
 	{
@@ -252,18 +183,74 @@ int GetCultures()
 		fread(&e.name, sizeof(e.name), 1, file);
 		fread(&e.area[0], sizeof(int), 1, file);
 		fread(&e.area[1], sizeof(int), 1, file);
-		if (e.area[1] > limit)
-		{
-			strcpy(currentCulture, e.name);
-			currentArea = e.area[1];
-			printf("В %d году производство СелХоз продукции %s превысило %d(%d)\n", years[1], currentCulture, limit, currentArea);
 
+		// Max. Культура '60
+		if (maxArea < e.area[0])						
+		{
+			strcpy(maxAreaCulture, e.name);
+			maxArea = e.area[0];
+		}	
+		// Культуры более 1000 га.
+		if (e.area[1] > 1000)							
+		{
+			fwrite(e.name, sizeof(e.name), 1, OverThousandArea);
+			fwrite(&e.area[0], sizeof(int), 1, OverThousandArea);
+			fwrite(&e.area[1], sizeof(int), 1, OverThousandArea);
 		}
+
+		if (e.area[0] < 1300)
+		{
+			strcpy(sorted[j].name, e.name);
+			sorted[j].area[0] = e.area[0];
+			sorted[j].area[1] = e.area[1];
+			j++;
+		}
+
 		i += sizeof(e.name) + 2 * sizeof(int);
 	}
+
+	for (int k = 0; k < j; k++)
+	{
+		for (int u = j-1; u > k; u--)
+		{
+			c1 = sorted[u - 1].name[0];
+			c2 = sorted[u].name[0];
+			
+			//printf("%c < %c", c1, c2);
+
+			if (c1 < c2)
+			{
+				//printf(" - true");
+				strcpy(tempName, sorted[u - 1].name);
+				strcpy(sorted[u - 1].name, sorted[u].name);
+				strcpy(sorted[u].name, tempName);
+				for (int v = 0; v < 2; v++)
+				{
+					t = sorted[u - 1].area[v];
+					sorted[u - 1].area[v] = sorted[u].area[v];
+					sorted[u].area[v] = t;
+				}
+			}
+			//printf("\n");
+		}
+	}
+
+	fwrite(maxAreaCulture, sizeof(maxAreaCulture), 1, MaxArea);
+	fwrite(&maxArea, sizeof(int), 1, MaxArea);
+
+	for (int i = 0; i < j; i++)
+	{
+		fwrite(&sorted[i].name, sizeof(sorted[i].name), 1, Sorted);
+		fwrite(&sorted[i].area[0], sizeof(int), 1, Sorted);
+		fwrite(&sorted[i].area[1], sizeof(int), 1, Sorted);
+	}
+	
 	fclose(file);
-	return 0;
+	fclose(MaxArea);
+	fclose(OverThousandArea);
+	fclose(Sorted);
 }
+
 int ValidateINT()
 {
 	char data[10];
